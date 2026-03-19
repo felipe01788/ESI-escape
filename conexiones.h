429650d0a64bfe67c6f70c566847450a1f4e87d1
cris-mdk <cris.guerreropina@alum.uca.es>
2026-03-19 17:28:35 +0100

    Add files via upload

--- conexiones.h
+++ conexiones.h
@@ -1,11 +1,11 @@
 #ifndef __CONEXIONES_H__
 #define __CONEXIONES_H__
-#define tamaño 10
+#define TAMANO 10
 typedef struct{
     int Id_conexion; //variable de tipo entero para indicar el id de la conexion que se vaya a usar
     int Id_origen; //variable de tipo entero para indicar de que sala proviene el jugador
     int Id_destino; //variable de tipo entero para indicar hacia que sala se dirige el jugador
-    char Estado[tamaño]; //vector de tipo caracter que indicar el estado de la conexion: Abierta o Cerrada
+    char Estado[TAMANO]; //vector de tipo caracter que indicar el estado de la conexion: Abierta o Cerrada
     int Cond; //variable para saber si la condicion necesaria para que se abra la condicion se cumple
 }conexion;
 //precondicion: el fichero conexiones.txt debe estar creado y con los datos necesarios para cargar las conexiones
