ebc2a63eb52832efb804af8bc455334724b08f79
cris-mdk <cris.guerreropina@alum.uca.es>
2026-03-19 15:13:59 +0100

    Add files via upload

---
+++ ficheros.h
@@ -0,0 +1,9 @@
+#ifndef __FICHEROS__
+#define __PALABRAS__
+#endif // __FICHEROS__
+
+void leer_fichero_char(char[] fichero,char[] cadena, int campo,int linea )
+void escribir_fichero_char(char[] fichero,char[] cadena, int campo, int linea)
+void escribir_fichero_int(char[] fichero,int numero, int campo, int linea)
+void leer_fichero_int(char[] fichero,int numero, int campo, int linea)
+#endif // __FICHEROS__
