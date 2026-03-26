8c184760a713e58291c9ddc3e1da57d8d1cb8aff
felipe01788 <165829252+felipe01788@users.noreply.github.com>
2026-03-26 13:41:33 +0100

    Refactor Objetos.h structure and comments

--- Objetos.h
+++ Objetos.h
@@ -4,7 +4,7 @@ typedef struct{
     int Id_obj;
     char Nomb_obj[31];
     char Describ[51];
-    int Localiz; //Hay que decidir aun como implementarlo
+    int Localiz; //Los objetos que van al inventario llevan un localizador 0. Este campo corresponde a un ID de sala.
 }objeto;
 //precondicion: el fichero objetos.txt debe estar creado y con los datos necesarios para cargar los objetos
 //postcondicion: se cargan los objetos del juego en un vector de tipo objeto
